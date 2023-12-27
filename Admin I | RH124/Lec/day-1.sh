da4292107002f113e9111b1f6458bb02ad32328e
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-12-27 17:39:14 +0200

    Create day-1.sh

---
+++ Admin I | RH124/Lec/day-1.sh
@@ -0,0 +1,41 @@
+    1 useradd mohamed
+    2  ls /home
+    3  adduser alaa
+    4  ls /home/
+    5  passwd alaa
+    6  ls /home/
+    7  cd /home/alaa
+    8  logout
+    9  history
+   10  history > myact.txt
+   11  cat myact.txt
+   12  6
+   13  f
+   14  cd /home/alaa
+   15  passwd alaa
+   16  su -u toba10
+   17  su 
+   18  sudo -i
+   19  su - toba10
+   20  su - toba10
+   21  su toba10
+   22  su -toba10
+   23  su -u 
+   24  su -
+   25  su -
+   26  su toba10
+   27  su -u toba10
+   28  su - toba10
+   29  su alaa
+   30  su - tob10
+   31  su - toba10
+   32  pwd
+   33  cd /home
+   34  pwd
+   35  cd /home/toba10
+   36  pwd
+   37  cd ..
+   38  cd ~
+   39  cd -
+   40  cd ..
+   41  cd ~
