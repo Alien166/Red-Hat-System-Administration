1f73e26532ccaff3a00962e2e19f769ffd8a8886
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-12-28 15:39:29 +0200

    Create one.sh

---
+++ Admin I | RH124/labs/Day-1/one.sh
@@ -0,0 +1,112 @@
+42  sudo -i
+   43  cd /home
+   44  mkdir dir1
+   45  cd dir1
+   46  mkdir 2   mkdir 3
+   47  pwd
+   48  cd dir2
+   49  cd dir 2
+   50  cd dir1
+   51  cd /dir1
+   52  mkdir dir11
+   53  mkdir dir12
+   54  cd dir11
+   55  touch file1
+   56  rm -i dir11
+   57  cd dir1
+   58  cd /dir1
+   59  cd ..
+   60  rm -r dir11
+   61  rmdir -p dir12
+   62  cp /etc/passwd   /home >mupasswd
+   63  mv oldpasswd  mupasswd
+   64  touch old passwd
+   65  mk oldpasswd mupasswd
+   66  mv oldpasswd mupasswd
+   67  rm -i old passwd
+   68  touch oldpasswd
+   69  mv oldpasswd mupasswd
+   70  cd /usr/bin
+   71  cd /home
+   72  cd /usr/bin
+   73  cd ~home
+   74  cd ~
+   75  cd ..
+   76  cd /home
+   77  head -n 4 /etc/passwd
+   78  tail-n 3 /etc/bin
+   79  tail -n 3 /etc/bin
+   80  tail -n 3 /etc/passwd
+   81  man -k passwd
+   82  man -k /etc/passwd
+   83  pwd
+   84  cd /dir1
+   85  cd dir1
+   86  cd ..
+   87  rm -r dir1
+   88  cd dir1
+   89  pwd
+   90  cd /usr/bin
+   91  pwd
+   92  cp /etc/passwd /home >mypasswd
+   93  cat mypasswd
+   94  touch newpasswd
+   95  mv mypasswd  newpasswd
+   96  cat newpasswd
+   97  more mypasswd
+   98  more newpasswd
+   99  clear
+  100  cd /home
+  101  mkdir dir1
+  102  cd dir1
+  103  mkdir dir11 dir 12
+  104  pwd
+  105  cat dir1
+  106  ls
+  107  rm dir11 dir 12
+  108  rm -r dir11 dor 12
+  109  rm -r dir11 dir 12
+  110  ls
+  111  cd ..
+  112  ls
+  113  cd dir
+  114  clear
+  115  cd dir1
+  116  mkdir dir11 dir12
+  117  ls -l
+  118  ls -ld 
+  119  ls
+  120  cd dir11
+  121  touch file1
+  122  cd ..
+  123  rm -r dir11
+  124  ls 
+  125  rmdir -p dir12
+  126  ls -l
+  127  cd ..
+  128  ls -l
+  129  cd /etc/passwd
+  130  cd /etc
+  131  cd ..
+  132  touch mypasswd
+  133  rm -i mypasswd
+  134  cd /homme
+  135  cd /home
+  136  touch mypasswd
+  137  cp /etc/passwd mypasswd
+  138  cat mypasswd
+  139  more mypasswd
+  140  less mypasswd
+  141  head -n 10 mypasswd
+  142  tail -n 4 mypasswd
+  143  touch newfile
+  144  mv mypasswd newfile
+  145  cat newfile
+  146  cat mypasswd
+  147  ls -l
+  148  cat
+  149  cat /etc/passwd
+  150  grap "passwd"  /etc/passwd
+  151  grep "passwd" /etc/passwd
+  152  history
+  153  history >day1.txt
