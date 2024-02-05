126784ac9c17fcd3aa54a98ef4237caff390db95
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2024-02-05 22:49:23 +0200

    Create lab3.bash

---
+++ Admin I | RH124/Chapter-3/lab3.bash
@@ -0,0 +1,48 @@
+    1  history
+    2  clear
+    3  cd Documents
+    4  mkdir project_plan
+    5  ls -l
+    6  touch /project_plan/season1.odf
+    7  cd project_plan
+    8  touch season1 season2
+    9  ls -la
+   10  ls -l
+   11  cd
+   12  touch tv_seasonX_eqisodeY{0..12}.ogg
+   13  ls tv*
+   14  touch mystery_chapter{1..12}.odf
+   15  ls myst*
+   16  cd /Videos
+   17  cd Videos
+   18  cd Videos
+   19  mkdir Videos
+   20  cd Videos
+   21  mkdir season{1..2}
+   22  ls -l
+   23  cd
+   24  mv tv_seasonX_eqisodeY /Videos/season1
+   25  mv tv_seasonX_eqisodeY* /Videos/season1
+   26  cd Documents
+   27  mkdir -p my_bestseller/chapters
+   28  ls -l
+   29  cd my_bestseller
+   30  ls -l
+   31  cd ..
+   32  cd -
+   33  mkdir {editor,changes,vacation}
+   34  ls -l
+   35  cd 
+   36  cd Documents/my_bestseller/chapters
+   37  mv ~/mystery_chapters*
+   38  mv ~/mystery_chapters*
+   39  cd
+   40  cat mystery_chapter1
+   41  cat mystery_chapter1.odf
+   42  mv mystery_chapter{7,8}.odf ../vacation
+   43  rm mystery*
+   44  rmdir changes
+   45  rm Documents/my_beseller/chapters/changes
+   46  cd Documents
+   47  touch lab3
+   48  history >lab3
