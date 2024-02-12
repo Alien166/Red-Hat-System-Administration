890403a062a9d09feb60d3da29c172970d5c178a
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2024-02-12 22:06:33 +0200

    Create three.sh

---
+++ Admin I | RH124/labs/Day-1/three.sh
@@ -0,0 +1,500 @@
+   10  nano file.txt
+   11  ./file.txt
+   12  nano file.tx
+   13  rm file.tx
+   14  nano file.txt
+   15  clear
+   16  ./file.txt
+   17  nano file.txt
+   18  ./file.txt
+   19  nano file.txt
+   20  ./file.txt
+   21  nano file.txt
+   22  ./file.txt
+   23  nano file.txt
+   24  sudo apt insatll unzip
+   25  su root
+   26  uname
+   27  uname -n
+   28  uname -a
+   29  date
+   30  date
+   31  nano file.txt
+   32  ./file.txt
+   33  nano filr.txt
+   34  nano file.txt
+   35  ./file.txt
+   36  nano file.txt
+   37  file.txt
+   38  ./file.txt
+   39  nano file.txt
+   40  ./file.txt
+   41  ./file.txt
+   42  nano file.txt
+   43  ./file.txt
+   44  nano file.txt
+   45  clear
+   46  ./file.txt
+   47  nano file.txt
+   48  ./file.txt
+   49  sudo apt insatll vim -y
+   50  sudo apt update
+   51  su root
+   52  sudo -i
+   53  cd -
+   54  cd ..
+   55  cd ..
+   56  pwd
+   57  cd ~
+   58  pwd
+   59  ls -ld home
+   60  ls -ld /home
+   61  ls /home
+   62  ls -a /home
+   63  ls -l /home
+   64  ls -f /home
+   65  ls -a /home
+   66  ls -l /home
+   67  ls -dl /home
+   68  ls -R /
+   69  ls -r /home
+   70  ls -R /home
+   71  touch file.txt
+   72  ./file.txt
+   73  cat file.txt
+   74  more file.txt
+   75   
+   76  b
+   77  b
+   78  grep "echo" file.txt
+   79  head -n 5 file.txt
+   80  head 5 file.txt
+   81  head -n 5 file.txt
+   82  tail -n 3 file.txt
+   83  tail +n 3 file.txt
+   84  tail -n 5 file.txt
+   85  head -n 5 file.txt
+   86  less file.txt
+   87  ls file.txt?
+   88  ls / file.txt?
+   89  ls /home file.txt?
+   90  touch file1.txt
+   91  whersis file1.txt
+   92  whereis file1.txt
+   93  file  file1.txt
+   94  cp file.txt  file1.txt
+   95  cat file1.txt
+   96  cp -i file1.txt
+   97  cp --help
+   98  clear
+   99  rm -i file1.txt
+  100  cat file1.txt
+  101  cat file.txt
+  102  clear
+  103  ls *file
+  104  ls f*
+  105  touch file1.txt
+  106  ls f*
+  107  touch friut.txt
+  108  touch apple.txt
+  109  ls f*
+  110  touch apple.txt
+  111  ls f*
+  112  ls *1
+  113  ls 1*
+  114  ls file.txt?
+  115  ls file1.txt
+  116  ls file1.txt?
+  117  ls *
+  118  ls .*
+  119  ls -l b*
+  120  ls m*
+  121  ls *f*
+  122  clear
+  123  cd ~DevOps
+  124  cd /DevOps
+  125  cd /home/toba10/DevOps
+  126  mkdir Docker
+  127  ls
+  128  ls -a
+  129  ls -ld
+  130  cd Docker
+  131  ls -ld
+  132  cd ..
+  133  ls -l
+  134  cd /Docker
+  135  cd Docker
+  136  cd ..
+  137  cd Sys Admin
+  138  cd SysAdmin
+  139  cd /home/toba10/DevOps/Sys-Admin
+  140  cd ..
+  141  cd ..
+  142  ls
+  143  pwd
+  144  cd DevOps
+  145  ls -l
+  146  ls -la
+  147  rmdir Docker
+  148  ls
+  149  ls -l
+  150  mkdir Docker
+  151  ls -l
+  152  pwd
+  153  cd /homw
+  154  cd /home
+  155  pwd
+  156  cd ..
+  157  cd ..
+  158  [200~sudo apt install ./zoom_amd64.deb~
+  159  sudo apt install ./zoom_amd64.deb
+  160  cd/home
+  161  cd /home
+  162  mkdir dir1
+  163  chmod +x dir1
+  164  mkdir -i dir1
+  165  mk -r dir1
+  166  clear
+  167  mkdir dir1
+  168  chmod +x dir1
+  169  ssh romteuser@remotehost
+  170  sude -i
+  171  clear
+  172  sudo -i
+  173  date +%R
+  174  date +%X
+  175  date +%x
+  176  file etc/passwd
+  177  claer
+  178  clear
+  179  file /etc/passwd
+  180  fie /etc/bin
+  181  file /home
+  182  wc -l /etc.group
+  183  wc -l /etc/passwd
+  184  wc -l /etc/passwd
+  185  wc -l /etc/group
+  186  sudo -i
+  187  history
+  188  wc -l /etc/group
+  189  lab grade cli-review
+  190  toba10 grade cli-review
+  191  passed --help
+  192  passwd --help
+  193  yarn install && yarn dev
+  194  sh -c "$(curl -fsSL https://raw.github.com/Cveinnt/LiveTerm/main/install/install.sh)"
+  195  nano live.txt
+  196  ./live.txt
+  197  chmod +X live.txt
+  198  ./live.txt
+  199  chmod +x live.txt
+  200  ./live.txt
+  201  sudo apt installyarn
+  202  npm install --global yarn
+  203  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  204  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+  205  git init
+  206  chmod +x file.txt
+  207  git init
+  208  git add *
+  209  clear
+  210  git config --global user.name "Alien166"
+  211  git config --globaluser.mail "abdowagieh@gmail.com"
+  212  git config --global user.mail "abdowagieh@gmail.com"
+  213  git add *
+  214  git commit -m"First commit"
+  215  git status
+  216  git remote add origin https://github.com/Alien166/devops-capstone-project.git
+  217  git status
+  218  git push -u origin master
+  219  git push -u origin master
+  220  cd /etc/passwd-
+  221  cat /etc/passwd-
+  222  clear
+  223  lab start cli-review
+  224  sudo -i
+  225  date
+  226  date +%R
+  227  file /home/student/zcat
+  228  file /etc/passwd
+  229  wc /etc/passwd
+  230  head /etc/passwd
+  231  tail /etc/passwd
+  232  history
+  233  tail /etc/passwd -n 5
+  234  tail -n 20/etc/passwd
+  235  tail -n 20 /etc/passwd
+  236  history
+  237  date +%R
+  238  file passwd
+  239  file /etc/passwd
+  240  wc file /etc/passwd
+  241  cd /home
+  242  touch day1
+  243  chmod +x day1
+  244  history >day1.txt
+  245  sudo -i
+  246  history
+  247  echo ~root
+  248  echo ~glob
+  249  date +%A
+  250  vmstat
+  251  vmstat 1 3
+  252  proc /file
+  253  sudo apt install obs-studio
+  254  sudo snap install obs-studio
+  255  sudo snap install vlc
+  256  ssh student@serverB
+  257  gnome
+  258  su -i
+  259  su -root
+  260  su --help
+  261  sudo -i
+  262  su -root
+  263  sudo -i
+  264  pwd
+  265  cd videos
+  266  cd DevOps
+  267  cd ..
+  268  pwd
+  269  cd /home/toba10/
+  270  pwd
+  271  cd ..
+  272  cd ..
+  273  pwd
+  274  cd /home/toba10
+  275  pwd
+  276  ls -l
+  277  ls -la
+  278  ls -l ~
+  279  touch cd /home/user/helloworld.py
+  280  touch cd /home/toba10/helloworld.py
+  281  cd Documents
+  282  mkdir project-1
+  283  ls
+  284  firefox
+  285  man cd
+  286  man ls
+  287  clear
+  288  man -k passwd
+  289  man -K passwd
+  290  clear
+  291  info ls
+  292  pinfo ls
+  293  ls pinfo
+  294  pinfo passwd
+  295  Pinfo passwd
+  296  sudo apt install pinfo
+  297  ls --help
+  298  ls /usr/share/doc/
+  299  ls /usr/share/doc/l
+  300  ls /usr/share/doc/lvm2
+  301  clear
+  302  ls /usr/share/doc/lvm2
+  303  ls usr/share/doc/Lvm2
+  304  ls /usr/share/doc/
+  305  ls /usr/share/doc/nano
+  306  cat /usr/share/doc/nano/README.gz
+  307  6c
+  308  q
+  309  q
+  310  q
+  311  clear
+  312  cd -
+  313  clear
+  314  history
+  315  ls /usr/share/doc/nano/
+  316  selte
+  317  cd /var/log/
+  318  ls -l
+  319  ls -l~
+  320  clear
+  321  cd 
+  322  pwd
+  323  pwd
+  324  cd /Documents
+  325  cd Documents
+  326  mkdir -r thesis/Chapter1 thesis/Chapter2 thesis/Chapter3
+  327  mkdir -R thisis/Chapter1 thisis/Chapter2  thisis/chapter3
+  328  mkdir -p this/chapter1 this/chapter2  this/chapter3
+  329  ls -l
+  330  ls -R
+  331  ls -r
+  332  ls -R this
+  333  cd
+  334  pwd
+  335  mkdir -p video watched
+  336  ls -l
+  337  cd video
+  338  ls
+  339  ls -l
+  340  cd
+  341  mkdir -p video/watched
+  342  cd video
+  343  ls -l
+  344  cd
+  345  rm -rf 
+  346  cd video
+  347  rm -rf video
+  348  cd
+  349  cd video
+  350  rm -r video
+  351  cd
+  352  rm -r video
+  353  cd video
+  354  cd /video/watched
+  355  cd
+  356  pwd
+  357  videoes
+  358  cd videos
+  359  cd Videos
+  360  cd Documents
+  361  touch newfile
+  362  ls -l
+  363  cat newfile
+  364  echo "Hello" >newfile
+  365  cat newfile
+  366  cd
+  367  cd Learning
+  368  touch filex
+  369  echo "World" >filex
+  370  cat filex
+  371  cd
+  372  li /Documents/newfile  /Learning/filex
+  373  ln /Documents/newfile  /Learning/filex
+  374  ln Documents/newfile  Learning/filex
+  375  ln Documents/newfile  Learning/newfile2
+  376  cd Documents
+  377  ls -l
+  378  ls -il
+  379  cd Learning
+  380  cd /Learning
+  381  cd
+  382  Learning
+  383  cd Learning
+  384  ls -l
+  385  ls -il
+  386  cd
+  387  df
+  388  cd
+  389  pwd
+  390  cd Documents
+  391  ls -l
+  392  ln -s ../newfile  filex
+  393  ls -l
+  394  ls -l filex
+  395  cd
+  396  pwd
+  397  cd -p --help
+  398  man cd -p
+  399  -p man
+  400  ln -s /etc  /home/toba10/configfiles
+  401  ls -l /etc
+  402  cd -p /home/toba10/configfiles
+  403  cd /home/toba10/configfiles
+  404  cat
+  405  cd Documents
+  406  mkdir dir ; cd dir
+  407  ls
+  408  touch alpha bravo charlie delta echo able baker cast dog easy
+  409  ls -l
+  410  ls a*
+  411  ls *a*
+  412  ls [ac*]
+  413  ls[ac*]
+  414  ls [ac]*
+  415  ls [ac]
+  416  ls [ac]*
+  417  ls ???
+  418  ls ????
+  419  ls ?????
+  420  cd
+  421  cd ~root
+  422  echo ~root
+  423  echo ~toba10
+  424  echo ~dir
+  425  echo today is (date +%A)
+  426  echo today is $(date +%A)
+  427  echo ther's your \$home diectory $home
+  428  home
+  429  /
+  430  q
+  431  exist
+  432  q
+  433  history
+  434  clear
+  435  cd Documents
+  436  mkdir project_plan
+  437  ls -l
+  438  touch /project_plan/season1.odf
+  439  cd project_plan
+  440  touch season1 season2
+  441  ls -la
+  442  ls -l
+  443  cd
+  444  touch tv_seasonX_eqisodeY{0..12}.ogg
+  445  ls tv*
+  446  touch mystery_chapter{1..12}.odf
+  447  ls myst*
+  448  cd /Videos
+  449  cd Videos
+  450  cd Videos
+  451  mkdir Videos
+  452  cd Videos
+  453  mkdir season{1..2}
+  454  ls -l
+  455  cd
+  456  mv tv_seasonX_eqisodeY /Videos/season1
+  457  mv tv_seasonX_eqisodeY* /Videos/season1
+  458  cd Documents
+  459  mkdir -p my_bestseller/chapters
+  460  ls -l
+  461  cd my_bestseller
+  462  ls -l
+  463  cd ..
+  464  cd -
+  465  mkdir {editor,changes,vacation}
+  466  ls -l
+  467  cd 
+  468  cd Documents/my_bestseller/chapters
+  469  mv ~/mystery_chapters*
+  470  mv ~/mystery_chapters*
+  471  cd
+  472  cat mystery_chapter1
+  473  cat mystery_chapter1.odf
+  474  mv mystery_chapter{7,8}.odf ../vacation
+  475  rm mystery*
+  476  rmdir changes
+  477  rm Documents/my_beseller/chapters/changes
+  478  cd Documents
+  479  touch lab3
+  480  history >lab3
+  481  cd
+  482  firefox /home/share/doc
+  483  vi /etc/passwd
+  484  lab_file = edit_lab.txt
+  485  ls -la > $lab_file
+  486  vim $lab_file
+  487  id
+  488  su - root
+  489  id root
+  490  id alaa
+  491  ls -ld
+  492  cd Documents
+  493  ls -ls
+  494  ls -ld
+  495  cd
+  496  ps -au
+  497  cat etc/passwd
+  498  cat /etc/passwd
+  499  cat /etc/group
+  500  su -
+  501  cat /etc/passwd
+  502  nano /etc/passwd
+  503  su -
+  504  cat /etc/passwd
+  505  clear
+  506  mkdir /users
+  507  su - 
+  508  cd Documents
+  509  history >lab-2
