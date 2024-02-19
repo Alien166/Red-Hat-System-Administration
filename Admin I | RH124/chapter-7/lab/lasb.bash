de513d602810eeafad38e769df3d6befd28af4f4
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2024-02-19 13:43:24 +0200

    Create lasb.bash

---
+++ Admin I | RH124/chapter-7/lab/lasb.bash
@@ -0,0 +1,500 @@
+    5  ./live.txt
+    6  chmod +x live.txt
+    7  ./live.txt
+    8  sudo apt installyarn
+    9  npm install --global yarn
+   10  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+   11  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+   12  git init
+   13  chmod +x file.txt
+   14  git init
+   15  git add *
+   16  clear
+   17  git config --global user.name "Alien166"
+   18  git config --globaluser.mail "abdowagieh@gmail.com"
+   19  git config --global user.mail "abdowagieh@gmail.com"
+   20  git add *
+   21  git commit -m"First commit"
+   22  git status
+   23  git remote add origin https://github.com/Alien166/devops-capstone-project.git
+   24  git status
+   25  git push -u origin master
+   26  git push -u origin master
+   27  cd /etc/passwd-
+   28  cat /etc/passwd-
+   29  clear
+   30  lab start cli-review
+   31  sudo -i
+   32  date
+   33  date +%R
+   34  file /home/student/zcat
+   35  file /etc/passwd
+   36  wc /etc/passwd
+   37  head /etc/passwd
+   38  tail /etc/passwd
+   39  history
+   40  tail /etc/passwd -n 5
+   41  tail -n 20/etc/passwd
+   42  tail -n 20 /etc/passwd
+   43  history
+   44  date +%R
+   45  file passwd
+   46  file /etc/passwd
+   47  wc file /etc/passwd
+   48  cd /home
+   49  touch day1
+   50  chmod +x day1
+   51  history >day1.txt
+   52  sudo -i
+   53  history
+   54  echo ~root
+   55  echo ~glob
+   56  date +%A
+   57  vmstat
+   58  vmstat 1 3
+   59  proc /file
+   60  sudo apt install obs-studio
+   61  sudo snap install obs-studio
+   62  sudo snap install vlc
+   63  ssh student@serverB
+   64  gnome
+   65  su -i
+   66  su -root
+   67  su --help
+   68  sudo -i
+   69  su -root
+   70  sudo -i
+   71  pwd
+   72  cd videos
+   73  cd DevOps
+   74  cd ..
+   75  pwd
+   76  cd /home/toba10/
+   77  pwd
+   78  cd ..
+   79  cd ..
+   80  pwd
+   81  cd /home/toba10
+   82  pwd
+   83  ls -l
+   84  ls -la
+   85  ls -l ~
+   86  touch cd /home/user/helloworld.py
+   87  touch cd /home/toba10/helloworld.py
+   88  cd Documents
+   89  mkdir project-1
+   90  ls
+   91  firefox
+   92  man cd
+   93  man ls
+   94  clear
+   95  man -k passwd
+   96  man -K passwd
+   97  clear
+   98  info ls
+   99  pinfo ls
+  100  ls pinfo
+  101  pinfo passwd
+  102  Pinfo passwd
+  103  sudo apt install pinfo
+  104  ls --help
+  105  ls /usr/share/doc/
+  106  ls /usr/share/doc/l
+  107  ls /usr/share/doc/lvm2
+  108  clear
+  109  ls /usr/share/doc/lvm2
+  110  ls usr/share/doc/Lvm2
+  111  ls /usr/share/doc/
+  112  ls /usr/share/doc/nano
+  113  cat /usr/share/doc/nano/README.gz
+  114  6c
+  115  q
+  116  q
+  117  q
+  118  clear
+  119  cd -
+  120  clear
+  121  history
+  122  ls /usr/share/doc/nano/
+  123  selte
+  124  cd /var/log/
+  125  ls -l
+  126  ls -l~
+  127  clear
+  128  cd 
+  129  pwd
+  130  pwd
+  131  cd /Documents
+  132  cd Documents
+  133  mkdir -r thesis/Chapter1 thesis/Chapter2 thesis/Chapter3
+  134  mkdir -R thisis/Chapter1 thisis/Chapter2  thisis/chapter3
+  135  mkdir -p this/chapter1 this/chapter2  this/chapter3
+  136  ls -l
+  137  ls -R
+  138  ls -r
+  139  ls -R this
+  140  cd
+  141  pwd
+  142  mkdir -p video watched
+  143  ls -l
+  144  cd video
+  145  ls
+  146  ls -l
+  147  cd
+  148  mkdir -p video/watched
+  149  cd video
+  150  ls -l
+  151  cd
+  152  rm -rf 
+  153  cd video
+  154  rm -rf video
+  155  cd
+  156  cd video
+  157  rm -r video
+  158  cd
+  159  rm -r video
+  160  cd video
+  161  cd /video/watched
+  162  cd
+  163  pwd
+  164  videoes
+  165  cd videos
+  166  cd Videos
+  167  cd Documents
+  168  touch newfile
+  169  ls -l
+  170  cat newfile
+  171  echo "Hello" >newfile
+  172  cat newfile
+  173  cd
+  174  cd Learning
+  175  touch filex
+  176  echo "World" >filex
+  177  cat filex
+  178  cd
+  179  li /Documents/newfile  /Learning/filex
+  180  ln /Documents/newfile  /Learning/filex
+  181  ln Documents/newfile  Learning/filex
+  182  ln Documents/newfile  Learning/newfile2
+  183  cd Documents
+  184  ls -l
+  185  ls -il
+  186  cd Learning
+  187  cd /Learning
+  188  cd
+  189  Learning
+  190  cd Learning
+  191  ls -l
+  192  ls -il
+  193  cd
+  194  df
+  195  cd
+  196  pwd
+  197  cd Documents
+  198  ls -l
+  199  ln -s ../newfile  filex
+  200  ls -l
+  201  ls -l filex
+  202  cd
+  203  pwd
+  204  cd -p --help
+  205  man cd -p
+  206  -p man
+  207  ln -s /etc  /home/toba10/configfiles
+  208  ls -l /etc
+  209  cd -p /home/toba10/configfiles
+  210  cd /home/toba10/configfiles
+  211  cat
+  212  cd Documents
+  213  mkdir dir ; cd dir
+  214  ls
+  215  touch alpha bravo charlie delta echo able baker cast dog easy
+  216  ls -l
+  217  ls a*
+  218  ls *a*
+  219  ls [ac*]
+  220  ls[ac*]
+  221  ls [ac]*
+  222  ls [ac]
+  223  ls [ac]*
+  224  ls ???
+  225  ls ????
+  226  ls ?????
+  227  cd
+  228  cd ~root
+  229  echo ~root
+  230  echo ~toba10
+  231  echo ~dir
+  232  echo today is (date +%A)
+  233  echo today is $(date +%A)
+  234  echo ther's your \$home diectory $home
+  235  home
+  236  /
+  237  q
+  238  exist
+  239  q
+  240  history
+  241  clear
+  242  cd Documents
+  243  mkdir project_plan
+  244  ls -l
+  245  touch /project_plan/season1.odf
+  246  cd project_plan
+  247  touch season1 season2
+  248  ls -la
+  249  ls -l
+  250  cd
+  251  touch tv_seasonX_eqisodeY{0..12}.ogg
+  252  ls tv*
+  253  touch mystery_chapter{1..12}.odf
+  254  ls myst*
+  255  cd /Videos
+  256  cd Videos
+  257  cd Videos
+  258  mkdir Videos
+  259  cd Videos
+  260  mkdir season{1..2}
+  261  ls -l
+  262  cd
+  263  mv tv_seasonX_eqisodeY /Videos/season1
+  264  mv tv_seasonX_eqisodeY* /Videos/season1
+  265  cd Documents
+  266  mkdir -p my_bestseller/chapters
+  267  ls -l
+  268  cd my_bestseller
+  269  ls -l
+  270  cd ..
+  271  cd -
+  272  mkdir {editor,changes,vacation}
+  273  ls -l
+  274  cd 
+  275  cd Documents/my_bestseller/chapters
+  276  mv ~/mystery_chapters*
+  277  mv ~/mystery_chapters*
+  278  cd
+  279  cat mystery_chapter1
+  280  cat mystery_chapter1.odf
+  281  mv mystery_chapter{7,8}.odf ../vacation
+  282  rm mystery*
+  283  rmdir changes
+  284  rm Documents/my_beseller/chapters/changes
+  285  cd Documents
+  286  touch lab3
+  287  history >lab3
+  288  cd
+  289  firefox /home/share/doc
+  290  vi /etc/passwd
+  291  lab_file = edit_lab.txt
+  292  ls -la > $lab_file
+  293  vim $lab_file
+  294  id
+  295  su - root
+  296  id root
+  297  id alaa
+  298  ls -ld
+  299  cd Documents
+  300  ls -ls
+  301  ls -ld
+  302  cd
+  303  ps -au
+  304  cat etc/passwd
+  305  cat /etc/passwd
+  306  cat /etc/group
+  307  su -
+  308  cat /etc/passwd
+  309  nano /etc/passwd
+  310  su -
+  311  cat /etc/passwd
+  312  clear
+  313  mkdir /users
+  314  su - 
+  315  cd Documents
+  316  history >lab-2
+  317  ls -l
+  318  cat lab-2
+  319  cd
+  320  sudo - root
+  321  su - root
+  322  userdel alaa
+  323  su - root
+  324  useradd ahmed
+  325  id
+  326  su - 
+  327  id root
+  328  id toba10
+  329  id ahmed
+  330  ls -l Documents
+  331  ps -au
+  332  cat /etc/group
+  333  usermod -aG ahmed Hr
+  334  usermod -aG ahmed HR
+  335  cat /etc/group
+  336  usermod -aG HR ahmed
+  337  su - root
+  338  sudo cat /etc/sudoers
+  339  date -d "+30 days" +%F
+  340  PASS_MAX_DAYS
+  341  PASS_MAX_DAYS 30
+  342  ls -l /bin/passwd
+  343  ls -l
+  344  chmod +x file
+  345  ls -l
+  346  chmod +s file
+  347  ls -l
+  348  chmod u+s file
+  349  ls -l
+  350  ls -l /bin/passwd
+  351  chmod U+s /bin/passwd
+  352  chmod u+s /bin/passwd
+  353  ls -l /bin/passwd
+  354  ls -l
+  355  chmod u+s myfile
+  356  ls -l
+  357  cat /etc/group
+  358  chgrp Hr Downloads
+  359  chgrp HR Downloads
+  360  ls -l
+  361  su -
+  362  chmod 755 Downloads
+  363  ls -l
+  364  ls -l /home/toba10
+  365  chown toba10:HR Downloads
+  366  su -
+  367  chgrp HR Downloads
+  368  chmod 777 Downloads
+  369  ls -l /homw/tob
+  370  ls -l /homw/toba10
+  371  ls -l/home/toba10
+  372  ls -l /home/toba10
+  373  chown toba10:HR Downloads
+  374  chmod g+s Downloads
+  375  ls -l Downloads/
+  376  ls -l 
+  377  ifconfig
+  378  su -
+  379  sudo apt install net-tools
+  380  fig
+  381  ifconfig
+  382  sudo apt install openssh-server
+  383  sudo systemctl status ssh
+  384  sudo ufw allow ssh
+  385  idconfig
+  386  ibconfig
+  387  ipconfig
+  388  iconfig
+  389  history
+  390  ifconfig
+  391  chmod o+t Downloads
+  392  ls -l
+  393  chmod -x Downloads
+  394  ls -l
+  395  chmod +x Downloads
+  396  ls -l
+  397  chmod u+s Downloads
+  398  ls -l
+  399  chmod -x downloads
+  400  chmod -x Downloads
+  401  ls -l
+  402  chmod +x Downloads
+  403  ls -l
+  404  chmod 4755 file
+  405  ls -l
+  406  chmod 5555 pictures
+  407  chmod 7555 Pictures
+  408  ls -l
+  409  chmod 755 Pictures
+  410  ls -l
+  411  su - ahmed
+  412  ls -l
+  413  umask
+  414  umask 0022
+  415  umask
+  416  touch filex
+  417  ls -l
+  418  umask 0002
+  419  umask
+  420  vi /etc/bashrc
+  421  cat /etc/bashrc
+  422  cat /etc/profiles
+  423  cat /etc/bash.bashrc
+  424  vi /etc/bash.bashrc
+  425  vi /etc/bash_completion
+  426  sudo systemctl ssh
+  427  sudo systemctl status ssh
+  428  sudo sudo ufw allow ssh
+  429  ifconfig
+  430  history
+  431  sudo systemctl status ssh
+  432  ifconfig
+  433  ls -ld Downloads
+  434  ls -lhd Downloads
+  435  cd DevOps
+  436  mkdir system
+  437  touch file
+  438  ls -l
+  439  rm -f file
+  440  ls -l
+  441  cd /system
+  442  cd system
+  443  mkdir sys
+  444  touch file
+  445  ls -l
+  446  chmod 777 system
+  447  cd ..
+  448  chmod system
+  449  chmod 777 system
+  450  ls -l
+  451  cd system
+  452  ls -l
+  453  cd ..
+  454  chmod 777 system -R
+  455  ls -l
+  456  cd system
+  457  ls -l
+  458  vi file
+  459  ./file
+  460  chmod 755 file
+  461  ls -l
+  462  ./file
+  463  chmod ugo+rwx file
+  464  ls -l
+  465  chmod u+rw g+rx o+r filw
+  466  chmod u+rw g+rx o+r file
+  467  ls -l
+  468  chmod u+rw,g+r,o-r file
+  469  ls -l
+  470  chmod ugo=rw file
+  471  ls -l
+  472  chmod a=rx file
+  473  ls -l
+  474  chmod w+ file
+  475  chmod +w file
+  476  ls -l
+  477  chmod -w file
+  478  ls -l
+  479  cd 
+  480  ls -l /bin/passwd
+  481  cd DevOps/system
+  482  ls -l
+  483  chmod u+s file
+  484  ls -l
+  485  chmod 755 file
+  486  ls -l
+  487  chmod u+s file
+  488  ls -l
+  489  cd ..
+  490  ls -l
+  491  cd
+  492  cat /etc/grouop
+  493  cat /etc/gruop
+  494  cat /etc/group
+  495  useradd -u 1003 ail
+  496  umask
+  497  umask 0022
+  498  umask
+  499  logout
+  500  umask
+  501  vi etc/bash.bashrc
+  502  history
+  503  cd DevOps/system
+  504  history >file
