99063ef57d6521c0ae3f77582324b95e50397059
Alien166 <abdowagieh@gmail.com>
2024-05-24 19:09:50 +0300

    Ch-1

---
+++ Admin I | RH143/ch-1-improving-command-line-productivity/ch_2-Admin_2.bash
@@ -0,0 +1,22 @@
+  386  grep ^# anaconda-ks.cfg 
+  387  nano sr
+  388  ./sr
+  389  ls sr
+  390  chmod 777 sr
+  391  ./sr
+  392  chmod 777 ./sr
+  393  ./sr
+  394  chmod +x sr
+  395  ./sr
+  396  nano sr
+  397  /.src
+  398  ls
+  399  ./sr
+  400  ./Sr
+  401  echo $PATH
+  402  pwd
+  403  export path="$PATH":/root
+  404  echo $PATH
+  405  export PATH="$PATH:/root"
+  406  echo $PATH
+
